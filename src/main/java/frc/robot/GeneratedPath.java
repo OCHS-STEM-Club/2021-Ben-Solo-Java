73c9650954c7e1654b84ea11f85bf9e0be244d52
Anirudhrajp <78001283+Anirudhrajp@users.noreply.github.com>
2021-03-06 11:19:52 -0600

    3/6 Check In
    Bounce Path

--- src/main/java/frc/robot/GeneratedPath.java
+++ src/main/java/frc/robot/GeneratedPath.java
@@ -5,16 +5,16 @@ public class GeneratedPath {
   // segments: 10
   // heading mode: cumulative
   public static LinearSegment[] MAIN = new LinearSegment[] {
-    new LinearSegment(0.00, 50.00),
-    new LinearSegment(-80.54, 60.83),
-    new LinearSegment(73.74, 125.00),
-    new LinearSegment(-0.00, 50.00),
-    new LinearSegment(-87.61, 120.10),
-    new LinearSegment(85.24, 120.42),
-    new LinearSegment(-0.00, 75.00),
-    new LinearSegment(-87.61, 120.10),
-    new LinearSegment(80.54, 60.83),
-    new LinearSegment(-0.00, 35.00)
+    new LinearSegment(0.00, 295.00),
+    new LinearSegment(90.00, 30.00),
+    new LinearSegment(180.00, 295.00),
+    new LinearSegment(90.00, 30.00),
+    new LinearSegment(0.00, 295.00),
+    new LinearSegment(90.00, 30.00),
+    new LinearSegment(180.00, 295.00),
+    new LinearSegment(90.00, 30.00),
+    new LinearSegment(0.00, 295.00),
+    new LinearSegment(-157.86, 318.47)
   };
 
 }
